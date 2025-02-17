140476a6c6e2b77bb5b4d674a078be1649942144
ferdisaputro <ferdisaputro1@gmail.com>
2025-02-17 13:57:18 +0700

    third commit

--- tugas-akhir/silab/resources/views/components/container.blade.php
+++
@@ -1,7 +0,0 @@
-@php
-   $classes = "max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-8 px-3"
-@endphp
-
-<div {{ $attributes->merge(['class' => $classes]) }}>
-   {{ $slot }}
-</div>
--- tugas-akhir/silab/resources/views/components/text/page-title.blade.php
+++
@@ -1,4 +0,0 @@
-<h3 {{ $attributes->merge(['class' => 'text-xl font-semibold ms-3 inline-flex items-center gap-3']) }}>
-    {{ $slot }}
-    <x-loading.circle wire:loading></x-loading.circle>
-</h3>
